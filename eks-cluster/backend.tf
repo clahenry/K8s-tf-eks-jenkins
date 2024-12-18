70622503145666888ece6b6dbb90c56238eea5f2
clahenry <henryclarence@hotmail.com>
2024-12-18 18:10:56 -0500

    Update backend.tf
    update bucket name

--- eks-cluster/backend.tf
+++ eks-cluster/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jensins-kubernetes-app-2024-v2"
+    bucket = "jenkins-terraform-kubernetes-app-2024-v2-henry"
     region = "us-east-1"
     key    = "eks/terraform.tfstate"
   }
